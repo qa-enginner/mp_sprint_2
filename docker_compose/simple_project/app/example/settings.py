6a3eebfff3354a6cb5b38c4da4fb4bdbba921fcc
Александр Бахарев <mts490702@yandex.ru>
2025-12-24 08:44:54 +0300

    Ready for review

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'  #os.getenv('DEBUG', False) == 'True'
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
